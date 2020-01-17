bbac0b827c689374fc724ef4e1d31bf6090dcdb4
usama <bozavlado@gmail.com>
2020-01-17 12:17:04 +0100

    par

--- scripts/deepnano2_caller.py
+++ scripts/deepnano2_caller.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import datetime
 import deepnano2
+from multiprocessing import Pool
 
 def med_mad(x, factor=1.4826):
     """
@@ -22,39 +23,59 @@ def rescale_signal(signal):
     signal /= mad
     return signal
 
-parser = argparse.ArgumentParser(description='Fast caller for ONT reads')
-
-parser.add_argument('--directory', type=str, nargs='*', help='One or more directories with reads')
-parser.add_argument('--reads', type=str, nargs='*', help='One or more read files')
-parser.add_argument("--output", type=str, required=True, help="Output FASTA file name")
-
-args = parser.parse_args()
-
-files = args.reads if args.reads else []
-if args.directory:
-    for directory_name in args.directory:
-        files += [os.path.join(directory_name, fn) for fn in os.listdir(directory_name)]
-
-if len(files) == 0:
-    print("Zero input reads, nothing to do.")
-    sys.exit()
-
-fout = open(args.output, "w")
-
-for fn in files:
-    start = datetime.datetime.now()
-    with get_fast5_file(fn, mode="r") as f5:
+def call_file(filename):
+    out = []
+    with get_fast5_file(filename, mode="r") as f5:
         for read in f5.get_reads():
             read_id = read.get_read_id()
             signal = read.get_raw_data()
             signal = rescale_signal(signal)
 
-            t1 = datetime.datetime.now()
-            base_call = deepnano2.call_raw_signal(signal)
-            t2 = datetime.datetime.now()
-            print(">%s" % read_id, file=fout)
-            print(base_call, file=fout)
-            fout.flush()
-            print("done", read_id, t1 - start, t2 - start, datetime.datetime.now() - start)
+            out.append((read_id, deepnano2.call_raw_signal(signal)))
+    return out
 
-fout.close()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fast caller for ONT reads')
+
+    parser.add_argument('--directory', type=str, nargs='*', help='One or more directories with reads')
+    parser.add_argument('--reads', type=str, nargs='*', help='One or more read files')
+    parser.add_argument("--output", type=str, required=True, help="Output FASTA file name")
+    parser.add_argument("--threads", type=int, default=1, help="Number of threads for basecalling")
+
+    args = parser.parse_args()
+
+    assert args.threads >= 1
+
+    files = args.reads if args.reads else []
+    if args.directory:
+        for directory_name in args.directory:
+            files += [os.path.join(directory_name, fn) for fn in os.listdir(directory_name)]
+
+    if len(files) == 0:
+        print("Zero input reads, nothing to do.")
+        sys.exit()
+
+    fout = open(args.output, "w")
+
+    if args.threads <= 1:
+        done = 0
+        for fn in files:
+            start = datetime.datetime.now()
+            for read_id, basecall in call_file(fn):
+                print(">%s" % read_id, file=fout)
+                print(basecall, file=fout)
+                done += 1
+                print("done %d/%d" % (done, len(files)), read_id, datetime.datetime.now() - start)
+
+    else:
+        pool = Pool(args.threads)
+        done = 0
+        for out in pool.imap_unordered(call_file, files):
+            for read_id, basecall in out:
+                print(">%s" % read_id, file=fout)
+                print(basecall, file=fout)
+                done += 1
+                print("done %d/%d" % (done, len(files)), read_id)
+    
+    fout.close()
+
